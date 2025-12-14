730b7891d215aeea4782f991c47b992593b40089
bpsaikoushik-jpg <bpsaikoushik@gmail.com>
2025-12-14 12:51:51 +0530

    Delete LoanAccount.java

--- LoanAccount.java
+++
@@ -1,9 +0,0 @@
-package BankingSystem.src;
-
-public class LoanAccount {
-
-    public static double calculateCompoundInterest(double p, double r, int t) {
-        return p * Math.pow(1 + r / 100, t);
-    }
-}
-
